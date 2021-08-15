918bc76c1abb0de869ceb3aaa126383adb021f67
Evan <evan40907870@gmail.com>
2021-08-15 10:20:10 +0800

    家用商品更新

--- inhome.js
+++ inhome.js
@@ -1,12 +1,70 @@
 $(".runb").click(function(){
     $(".runt").slideToggle();
+    $(".runp").slideToggle();
 });
 $(".bikeb").click(function(){
     $(".biket").slideToggle();
+    $(".bikep").slideToggle();
 });
 $(".powerb").click(function(){
     $(".powert").slideToggle();
+    $(".powerp").slideToggle();
 });
 $(".otherb").click(function(){
     $(".othert").slideToggle();
+    $(".otherp").slideToggle();
+});
+$(".run1").click(function(){
+    $(".runp").css('background-image','url(/inhomepic/run1.jpg)');
+});
+$(".run2").click(function(){
+    $(".runp").css('background-image','url(/inhomepic/run2.jpg)');
+});
+$(".run3").click(function(){
+    $(".runp").css('background-image','url(/inhomepic/run3.jpg)');
+});
+$(".run4").click(function(){
+    $(".runp").css('background-image','url(/inhomepic/run4.jpg)');
+});
+$(".run5").click(function(){
+    $(".runp").css('background-image','url(/inhomepic/run5.jpg)');
+});
+$(".run6").click(function(){
+    $(".runp").css('background-image','url(/inhomepic/run6.jpg)');
+});
+$(".run7").click(function(){
+    $(".runp").css('background-image','url(/inhomepic/run7.jpg)');
+});
+$(".bike1").click(function(){
+    $(".bikep").css('background-image','url(/inhomepic/bike1.jpg)');
+});
+$(".bike2").click(function(){
+    $(".bikep").css('background-image','url(/inhomepic/bike2.jpg)');
+});
+$(".bike3").click(function(){
+    $(".bikep").css('background-image','url(/inhomepic/bike3.jpg)');
+});
+$(".bike4").click(function(){
+    $(".bikep").css('background-image','url(/inhomepic/bike4.jpg)');
+});
+$(".bike5").click(function(){
+    $(".bikep").css('background-image','url(/inhomepic/bike5.jpg)');
+});
+$(".power1").click(function(){
+    $(".powerp").css('background-image','url(/inhomepic/power1.jpg)');
+});
+$(".power2").click(function(){
+    $(".powerp").css('background-image','url(/inhomepic/power2.jpg)');
+});
+$(".power3").click(function(){
+    $(".powerp").css('background-image','url(/inhomepic/power3.jpg)');
+});
+$(".other1").click(function(){
+    $(".otherp").css('background-image','url(/inhomepic/other1.png)');
+});
+$(".other2").click(function(){
+    $(".otherp").css('background-image','url(/inhomepic/other2.jpg)');
+});
+$(".other3").click(function(){
+    $(".otherp").css('background-image','url(/inhomepic/other3.png)');
 });
